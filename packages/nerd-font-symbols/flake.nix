74bf786152576611a8671e8717606c63cbb4adc0
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-03-12 16:11:33 +0100

    feat: cleanup code now working

--- packages/nerd-font-symbols/flake.nix
+++ packages/nerd-font-symbols/flake.nix
@@ -8,7 +8,42 @@
       let pkgs = import nixpkgs { inherit system; };
       in {
         packages = flake-utils.lib.flattenTree {
-          nerd-font-symbols = import ./default.nix pkgs;
+          nerd-font-symbols = let
+            version = "2.1.0";
+            fileName = "Nerd-Fonts-Symbols.ttf";
+            src = pkgs.fetchurl {
+              name = fileName;
+              url =
+                "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v${version}/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf";
+              sha256 = "sha256-VpnnL91Bq5e8Hnhu9sCoskGOiDiAxWEDvuiKRB23Hh0=";
+            };
+          in pkgs.stdenv.mkDerivation {
+            inherit version;
+            inherit src;
+
+            pname = "nerd-font-symbols";
+
+            dontUnpack = true;
+
+            sourceRoute = ".";
+
+            installPhase = ''
+              install -D ${src} $out/share/fonts/truetype/NerdFonts/${fileName}
+            '';
+
+            meta = {
+              description =
+                "Iconic font aggregator, collection, & patcher. 3,600+ icons, 50+ patched fonts";
+              longDescription = ''
+                Nerd Fonts is a project that attempts to patch as many developer targeted
+                and/or used fonts as possible. The patch is to specifically add a high
+                number of additional glyphs from popular 'iconic fonts' such as Font
+                Awesome, Devicons, Octicons, and others.
+              '';
+              homepage = "https://nerdfonts.com/";
+              license = pkgs.lib.licenses.mit;
+            };
+          };
         };
         defaultPackage = self.packages.${system}.nerd-font-symbols;
       });

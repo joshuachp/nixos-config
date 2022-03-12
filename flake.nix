a0a8722e5a670c9567f3a7519cdd353b4157e7a2
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-03-12 21:13:07 +0100

    feat: enable network with systemd-resolved

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
         ./desktop
         ./develop
         ./system/nixos
+        ./system/common/network
         nixos-hardware.nixosModules.common-cpu-amd
         nixos-hardware.nixosModules.common-gpu-amd
         nixos-hardware.nixosModules.common-pc-laptop
---
+++ system/common/network/default.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  services.resolved = {
+    enable = true;
+    fallbackDns = [
+      "45.90.28.0#John--Blackbox-4ee8d8.dns1.nextdns.io"
+      "2a07:a8c0::#John--Blackbox-4ee8d8.dns1.nextdns.io"
+      "45.90.30.0#John--Blackbox-4ee8d8.dns2.nextdns.io"
+      "2a07:a8c1::#John--Blackbox-4ee8d8.dns2.nextdns.io"
+      "2a07:a8c1::#John--Blackbox-4ee8d8.dns2.nextdns.io"
+    ];
+    extraConfig = ''
+      DNSOverTLS=opportunistic
+    '';
+  };
+
+  networking.networkmanager.dns = "systemd-resolved";
+}

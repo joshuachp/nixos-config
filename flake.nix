d8fa07f075583252ff5c30b6f7bbc18aff929d43
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-03-12 07:09:22 +0100

    feat: install nerdfont symbols

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
         ./desktop
         ./develop
         ./system/nixos
+        ./packages/nerdfont-symbols
         nixos-hardware.nixosModules.common-cpu-amd
         nixos-hardware.nixosModules.common-gpu-amd
         nixos-hardware.nixosModules.common-pc-laptop

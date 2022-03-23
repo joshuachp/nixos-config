87f426d80dfe06b74d94bfdbd7416e94d4a185fe
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-03-23 21:08:07 +0100

    feat: simplify reverse prime configuration

--- system/nixos/video-configuration.nix
+++ system/nixos/video-configuration.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: {
-  services.xserver.videoDrivers = lib.mkAfter ["nvidia"];
+  services.xserver.videoDrivers = lib.mkAfter ["amdgpu" "nvidia"];
 
   hardware.nvidia = {
     powerManagement = {
@@ -23,55 +23,18 @@
     };
   };
 
+  services.xserver.deviceSection = ''
+    Option "AllowExternalGpus"
+  '';
+
+  services.xserver.serverLayoutSection = ''
+    Inactive "Device-nvidia[0]"
+    Option "AllowNVIDIAGPUScreens"
+  '';
+
   services.xserver.displayManager.sessionCommands = ''
     ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 1 0
   '';
 
   services.xserver.displayManager.gdm.wayland = false;
-
-  services.xserver.config = lib.mkForce ''
-    Section "ServerLayout"
-        Identifier      "Server Layout"
-        Screen          0 "Main Screen"
-    EndSection
-
-    Section "Screen"
-        Identifier      "Main Screen"
-        Device          "AMD Card"
-        GPUDevice       "Nvidia Card"
-        #Device          "Nvidia Card"
-        #GPUDevice       "AMD Card"
-        Monitor         "eDP"
-        Option          "AllowEmptyInitialConfiguration"
-    EndSection
-
-    Section "Monitor"
-        Identifier      "eDP"
-        Option          "Primary" "true"
-        Option          "Enable" "true"
-        Option          "DPMS" "true"
-    EndSection
-
-    Section "Monitor"
-        Identifier      "HDMI-1-0"
-        Option          "LeftOf" "eDP"
-        Option          "DPMS" "true"
-    EndSection
-
-    Section "Device"
-        Identifier      "AMD Card"
-        Driver          "amdgpu"
-        BusId           "PCI:5:0:0"
-        Option          "Monitor-eDP" "eDP"
-        Option          "DRI" "3"
-    EndSection
-
-    Section "Device"
-        Identifier      "Nvidia Card"
-        Driver          "nvidia"
-        BusId           "PCI:1:0:0"
-        Option          "Monitor-HDMI-1-0" "HDMI-1-0"
-        Option          "DRI" "3"
-    EndSection
-  '';
 }

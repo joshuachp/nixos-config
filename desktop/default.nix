34d054f421763ddeed105125cbea777a10c07187
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-04-09 15:12:29 +0200

    feat: disable zathura, broken after meson 6.0

--- desktop/default.nix
+++ desktop/default.nix
@@ -67,7 +67,8 @@
     spotify
     tdesktop
     thunderbird
-    zathura
+    # TODO: re-add, currently breaking for meason 6.0 update. Upstream already merged https://github.com/NixOS/nixpkgs/issues/167700
+    # zathura
 
     # Other
     pinentry-gnome

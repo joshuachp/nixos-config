f60199f8cc2ee3a5788f683a10fcd2de001f9768
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-04-28 13:24:19 +0200

    feat: overlay to update sumneko-lua-language-server

--- develop/lua.nix
+++ develop/lua.nix
@@ -1,4 +1,27 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  nixpkgs.overlays = [
+    (
+      self: super: {
+        sumneko-lua-language-server = super.sumneko-lua-language-server.overrideAttrs (old: let
+          version = "3.2.2";
+        in {
+          inherit version;
+          src = super.fetchFromGitHub {
+            owner = "sumneko";
+            repo = "lua-language-server";
+            rev = version;
+            sha256 = "sha256-wjn0yYHKNdm8kaNCrx5vif2U1N9+P9+rmwDGuNpBpLY=";
+            fetchSubmodules = true;
+          };
+        });
+      }
+    )
+  ];
+
   environment.systemPackages = with pkgs; [
     luaformatter
     sumneko-lua-language-server

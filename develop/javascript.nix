9962fe233dc40bf5ec3f10138c4efd145acdfde4
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-05-22 16:43:44 +0200

    feat: add svelte-language-server

--- develop/javascript.nix
+++ develop/javascript.nix
@@ -3,6 +3,7 @@
     nodejs
     nodePackages."@tailwindcss/language-server"
     nodePackages.prettier
+    nodePackages.svelte-language-server
     nodePackages.typescript
     nodePackages.typescript-language-server
     nodePackages.vscode-langservers-extracted

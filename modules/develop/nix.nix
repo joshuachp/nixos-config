3782b3bf6c2b8615ee609f91744319d7cd9dabc9
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-12-24 10:52:04 +0100

    feat(nix): install nil language server diff --git a/modules/develop/nix.nix b/modules/develop/nix.nix index de3c346..e8584ba 100644 --- a/modules/develop/nix.nix +++ b/modules/develop/nix.nix @@ -4,12 +4,14 @@  }: {    config = {      environment.systemPackages = with pkgs; [ +      # Formatters        nixfmt        nixpkgs-fmt        alejandra - +      # Language servers        rnix-lsp - +      nil +      # Linters        statix      ];    };

--- modules/develop/nix.nix
+++ modules/develop/nix.nix
@@ -4,12 +4,14 @@
 }: {
   config = {
     environment.systemPackages = with pkgs; [
+      # Formatters
       nixfmt
       nixpkgs-fmt
       alejandra
-
+      # Language servers
       rnix-lsp
-
+      nil
+      # Linters
       statix
     ];
   };

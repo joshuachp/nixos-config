fe01b498933080b71520a8f75daad8703d236b98
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-01-22 10:38:55 +0100

    feat(nix): use nix-direnv to cache evaluations

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -9,6 +9,9 @@
 
     environment.systemPackages = with pkgs; [
       cachix
+      # Nix shell evaluation on enter
+      direnv
+      nix-direnv
     ];
 
     # Enable flakes system wide, this will no loner be necessary in some future release
@@ -16,6 +19,20 @@
 
     nix.settings.auto-optimise-store = true;
 
+    # nix options for derivations to persist garbage collection
+    nix.settings = {
+      keep-outputs = true;
+      keep-derivations = true;
+    };
+    environment.pathsToLink = [
+      "/share/nix-direnv"
+    ];
+    # Support flakes in nix direnv
+    nixpkgs.overlays = [
+      (self: super: { nix-direnv = super.nix-direnv.override { enableFlakes = true; }; })
+    ];
+
+    # Periodically optimise the store
     nix.optimise = {
       automatic = true;
       dates = [ "weekly" ];

a36704464a9e9625d91918c2474aea5295b7ba6d
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-01-26 14:13:41 +0100

    feat(wireguard): add android peer

--- modules/wireguard/server.nix
+++ modules/wireguard/server.nix
@@ -37,16 +37,21 @@
             publicKey = config.privateConfig.wireguard.nixosPublicKey;
             allowedIPs = [ "10.0.0.1/32" "fdc9:281f:04d7:9ee9::1/128" ];
           }
-          # Nixos Work
-          {
-            publicKey = config.privateConfig.wireguard.nixosWorkPublicKey;
-            allowedIPs = [ "10.0.0.3/32" "fdc9:281f:04d7:9ee9::3/128" ];
-          }
           # Nixos Cloud
           # {
           #   publicKey = config.privateConfig.wireguard.nixosCloudPublicKey;
           #   allowedIPs = [ "10.0.0.2/32" "fdc9:281f:04d7:9ee9::2/128" ];
           # }
+          # Nixos Work
+          {
+            publicKey = config.privateConfig.wireguard.nixosWorkPublicKey;
+            allowedIPs = [ "10.0.0.3/32" "fdc9:281f:04d7:9ee9::3/128" ];
+          }
+          # Nixos Work
+          {
+            publicKey = config.privateConfig.wireguard.androidPublicKey;
+            allowedIPs = [ "10.0.0.4/32" "fdc9:281f:04d7:9ee9::4/128" ];
+          }
         ];
       };
     };

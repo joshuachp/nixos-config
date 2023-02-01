e48afc2e5ebe0f95ad5b378d9e5551fa1f28cdba
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-02-01 10:47:07 +0100

    feat(gpg): refactor and enable additional services

--- modules/gnupg.nix
+++ modules/gnupg.nix
@@ -1,22 +1,29 @@
 { config
 , pkgs
+, lib
 , ...
 }: {
   config = {
     programs = {
       # GPG
-      ssh.startAgent = false;
       gnupg.agent = {
         enable = true;
         enableSSHSupport = true;
+        enableExtraSocket = true;
         pinentryFlavor = "curses";
       };
+      ssh.startAgent = false;
     };
+
+    systemd.user.sockets.gpg-agent.listenStreams = [ "" "%t/gnupg/d.8jmbbcqh9gemi75at4554oo4/S.gpg-agent" ];
+    systemd.user.sockets.gpg-agent-ssh.listenStreams = [ "" "%t/gnupg/d.8jmbbcqh9gemi75at4554oo4/S.gpg-agent.ssh" ];
+    systemd.user.sockets.gpg-agent-extra.listenStreams = [ "" "%t/gnupg/d.8jmbbcqh9gemi75at4554oo4/S.gpg-agent.extra" ];
 
     environment.systemPackages = with pkgs; [
       gnupg
       pinentry
-      pinentry-curses
+      pinentry.curses
+      pinentry.tty
     ];
   };
 }

c538135894edda52b674fbefc128dcfa54532bee
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-02-01 10:49:54 +0100

    feat: add gcr package

--- modules/gnupg.nix
+++ modules/gnupg.nix
@@ -15,6 +15,8 @@
       ssh.startAgent = false;
     };
 
+    services.dbus.packages = [ pkgs.gcr ];
+
     systemd.user.sockets.gpg-agent.listenStreams = [ "" "%t/gnupg/d.8jmbbcqh9gemi75at4554oo4/S.gpg-agent" ];
     systemd.user.sockets.gpg-agent-ssh.listenStreams = [ "" "%t/gnupg/d.8jmbbcqh9gemi75at4554oo4/S.gpg-agent.ssh" ];
     systemd.user.sockets.gpg-agent-extra.listenStreams = [ "" "%t/gnupg/d.8jmbbcqh9gemi75at4554oo4/S.gpg-agent.extra" ];

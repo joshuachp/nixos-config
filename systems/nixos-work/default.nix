d196af9e5fd7e27be314f92b41c36a1a9b8155b0
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-02-10 09:19:30 +0100

    feat: install cryptsetup

--- systems/nixos-work/default.nix
+++ systems/nixos-work/default.nix
@@ -120,7 +120,11 @@
     yubikey-personalization
     # Phpstorm
     phpStormRemote
+
+    # Crypt
+    cryptsetup
   ];
+
 
   programs.java.package = pkgs.jetbrains.jdk;
   programs.java.enable = true;

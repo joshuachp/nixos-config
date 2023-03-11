98a83c38c615d05987a073df021fd75440a13ebb
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-11 16:03:01 +0100

    fix(nixos): remove unused xorg configuration and move to wayland
    The xorg configuration was buggy since it needed to manually set the provider
    output source. This is now done automatically by the nvidia driver and
    the wayland system.

--- systems/nixos/video-configuration.nix
+++ systems/nixos/video-configuration.nix
@@ -7,9 +7,7 @@
     services.xserver.exportConfiguration = true;
 
     services.xserver.videoDrivers = [
-      # "modesetting"
-      # "fbdev"
-      # "amdgpu",
+      "amdgpu"
       "nvidia"
     ];
 
@@ -28,77 +26,17 @@
 
 
     hardware.nvidia = {
-      #powerManagement = {
-      #  enable = true;
-      #  finegrained = true;
-      #};
-
       nvidiaPersistenced = true;
 
       modesetting.enable = true;
 
       prime = {
-        #offload.enable = true;
         sync.enable = true;
 
         amdgpuBusId = "PCI:5:0:0";
         nvidiaBusId = "PCI:1:0:0";
       };
     };
-
-    environment.etc."X11/xorg.conf.d/10-amdgpu.conf".text = ''
-      Section "OutputClass"
-          Identifier "AMDgpu"
-          MatchDriver "amdgpu"
-          Driver "amdgpu"
-      EndSection
-    '';
-
-    environment.etc."X11/xorg.conf.d/10-radeon.conf".text = ''
-      Section "OutputClass"
-          Identifier "Radeon"
-          MatchDriver "radeon"
-          Driver "radeon"
-      EndSection
-    '';
-
-    environment.etc."X11/xorg.conf.d/10-nvidia.conf".text = ''
-      Section "OutputClass"
-          Identifier "nvidia"
-          MatchDriver "nvidia-drm"
-          Driver "nvidia"
-          Option "AllowEmptyInitialConfiguration"
-          Option "SLI" "Auto"
-          Option "BaseMosaic" "on"
-      EndSection
-
-      Section "ServerLayout"
-          Identifier "layout"
-          Option "AllowNVIDIAGPUScreens"
-      EndSection
-    '';
-
-    services.xserver.serverLayoutSection = lib.mkForce "";
-
-    services.xserver.screenSection = ''
-      GPUDevice "Device-amdgpu[0]"
-    '';
-
-    services.xserver.drivers = [{
-      name = "amdgpu";
-      display = false;
-      modules = [ pkgs.xorg.xf86videoamdgpu ];
-      #screenSection = ''
-      #  GPUDevice "Device-nvidia[0]"
-      #'';
-    }];
-
-    services.xserver.displayManager.setupCommands = ''
-      ${pkgs.xorg.xrandr} --setprovideroutputsource 1 0
-      ${pkgs.xorg.xrandr} --auto
-    '';
-
-    services.xserver.displayManager.gdm.wayland = false;
 
     # Specialization for using the AMD as primary GPU
     specialisation.singleMonitor.configuration = {

3f31f6954a2f67cc89e6b1683ba5aefdb5c0d5c3
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-04-11 16:53:09 +0200

    feat(virt): use unstable version of qemu

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,24 +5,31 @@
 , note
 , pulseaudioMicState
 , fenix
+, nixpkgs-unstable
 , ...
 }: {
-  config = {
-    nixpkgs.overlays = [
-      fenix.overlays.default
+  config =
+    let
+      pkgsUnstable = import nixpkgs-unstable { inherit system; };
+    in
+    {
+      nixpkgs.overlays = [
+        fenix.overlays.default
 
-      (self: super: {
-        nerdfonts = super.nerdfonts.override {
-          # Only the symbols are needed
-          fonts = [ "NerdFontsSymbolsOnly" ];
-        };
+        (self: super: {
+          inherit (pkgsUnstable) qemu OVMFFull;
 
-        jumpOverlay = jump.packages.${system}.default;
-        rust-toolsOverlay = tools.packages.${system}.rust-tools;
-        shell-toolsOverlay = tools.packages.${system}.shell-tools;
-        noteOverlay = note.packages.${system}.default;
-        pulseaudioMicStateOverlay = pulseaudioMicState.packages.${system}.default;
-      })
-    ];
-  };
+          nerdfonts = super.nerdfonts.override {
+            # Only the symbols are needed
+            fonts = [ "NerdFontsSymbolsOnly" ];
+          };
+
+          jumpOverlay = jump.packages.${system}.default;
+          rust-toolsOverlay = tools.packages.${system}.rust-tools;
+          shell-toolsOverlay = tools.packages.${system}.shell-tools;
+          noteOverlay = note.packages.${system}.default;
+          pulseaudioMicStateOverlay = pulseaudioMicState.packages.${system}.default;
+        })
+      ];
+    };
 }

d2ce0c0b5bdc1e643c0a4df73e30ff820e0fe9f8
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-04-14 18:29:38 +0200

    feat(syncthing): add systemd user target for tray

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -4,5 +4,13 @@ _: {
       enable = true;
       tray.enable = true;
     };
+
+    # https://github.com/nix-community/home-manager/issues/2064
+    systemd.user.targets.tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = [ "graphical-session-pre.target" ];
+      };
+    };
   };
 }

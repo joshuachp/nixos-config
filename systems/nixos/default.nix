3a5d1592e03c2e9a56c85f61db2aae0e0feafa49
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-06-05 14:54:12 +0200

    feat(nixos): enable docker for development

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -47,6 +47,13 @@
       };
     };
 
+    # Enable docker
+    virtualisation.docker = {
+      enable = true;
+      # Only for dev
+      enableOnBoot = false;
+    };
+
     networking = {
       # The global useDHCP flag is deprecated, therefore explicitly set to false here.
       # Per-interface useDHCP will be mandatory in the future, so this generated config

d765cbbe963e0b1108743b40614a92b69f916d0d
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-07-17 09:43:36 +0200

    fix(plymouth): boot with systemd initrd in stage 1
    This fixes the password prompt not showing.

--- modules/nixos/plymouth.nix
+++ modules/nixos/plymouth.nix
@@ -24,5 +24,9 @@ in
     # Make the boot quiet (man kernel-command-line), should make the password prompt for the
     # LUCKS device
     boot.kernelParams = [ "quiet" ];
+
+    # NOTE: This could be buggy and some of the initrd.systemd options are unstable and subject to
+    #       change, but it's needed to start plymouth in stage 1
+    boot.initrd.systemd.enable = true;
   };
 }

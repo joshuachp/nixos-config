07a72b7a67189d4e13d73f31c5c1aa57d2946b6b
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-02-19 09:00:26 +0100

    feat(dns): add wireguard to custom hosts

--- modules/nixos/wireguard/server.nix
+++ modules/nixos/wireguard/server.nix
@@ -30,13 +30,14 @@
           settings = {
             interface = "wg0";
             address = dnsAddresses ++ [
+              "/argocd.k.joshuachp.dev/${clusterIp}"
               "/git.k.joshuachp.dev/${clusterIp}"
               "/home.k.joshuachp.dev/${clusterIp}"
+              "/kubeapi.k.joshuachp.dev/${clusterIp}"
               "/kubernetes-dashboard.k.joshuachp.dev/${clusterIp}"
               "/ntfy.k.joshuachp.dev/${clusterIp}"
               "/syncthing.k.joshuachp.dev/${clusterIp}"
               "/traefik.k.joshuachp.dev/${clusterIp}"
-              "/kubeapi.k.joshuachp.dev/${clusterIp}"
             ];
           };
         };

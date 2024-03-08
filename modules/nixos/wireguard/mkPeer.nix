1031ae4f0349eb91609f7f4bbeff0afff9d32d24
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-03-08 16:49:35 +0100

    WIP

--- modules/nixos/wireguard/mkPeer.nix
+++ modules/nixos/wireguard/mkPeer.nix
@@ -3,10 +3,11 @@ v:
 let
   port = toString v.wireguard.port;
   range = toString v.wireguard.range;
+  peers = v.wireguard.peers;
 in
 {
   inherit (v.wireguard) publicKey;
   endpoint = lib.mkIf (v.publicIpv4 != null) "${v.publicIpv4}:${port}";
-  allowedIPs = [ "${v.wireguard.addressIpv4}/${range}" ];
+  allowedIPs = [ "${v.wireguard.addressIpv4}/${range}" ] ++ peers;
   persistentKeepalive = 25;
 }

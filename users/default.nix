c4e3ceb2d141b05cc3bef5c92dd43eb129a39e7a
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-05-06 10:56:48 +0200

    feat(pipewire): disable jack

--- users/default.nix
+++ users/default.nix
@@ -20,6 +20,10 @@
         share-dir = {
           gid = 3001;
         };
+        # For network manager
+        netdev = {
+          gid = 106;
+        };
       };
 
       users.root.hashedPasswordFile = config.sops.secrets.users_passwords_root.path;

9a7385aa1814566bc65eadd096a8ecfaf0392409
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-06-27 17:33:03 +0200

    feat(nushell): configure nushell aliases and options

--- modules/homeManager/shell/aliases.nix
+++ modules/homeManager/shell/aliases.nix
@@ -30,53 +30,56 @@ let
     glo = "git log --oneline";
     grc = "git rebase --continue";
   };
+  ls = {
+    # Eza as ls
+    ls = "eza --group-directories-first --group --hyperlink";
+    ll = "ls -lh";
+    la = "ls -la";
+    lt = "eza --group-directories-first --group --tree";
+  };
+  aliases = {
+    # Better options for core packages
+    # Less intrusive than -i but still secure
+    rm = "rm -I";
+    q = "exit";
+    diff = "diff --color=auto";
+    grep = "grep --color=auto";
+    mv = "mv -i";
+    cp = "cp -i";
+    free = "free -h";
+
+    # Git
+    gpsup = "git push --set-upstream origin \"$(git branch --show-current)\"";
+
+    # Utils
+    cdtmp = "cd (mktemp -d)";
+    nvimdiff = "nvim -d";
+    open = "xdg-open";
+
+    # Config
+    chezmoi-cd = "cd \"$(chezmoi source-path)\"";
+
+    # Bluetooth
+    btoff = "bluetoothctl power off";
+
+    # Task warrior
+    taskw = "task project:work";
+  };
+  shAliases = {
+    # Utils
+    mkdate = "mkdir \"$(date +%Y-%m-%d)\"";
+  };
 in
 {
   config = {
-    home.shellAliases = {
-      # Better options for core packages
-      # Less intrusive than -i but still secure
-      rm = "rm -I";
-      q = "exit";
-      diff = "diff --color=auto";
-      grep = "grep --color=auto";
-      mv = "mv -i";
-      cp = "cp -i";
-      free = "free -h";
-
-      # Eza as ls
-      ls = "eza --group-directories-first --group";
-      ll = "ls -lh";
-      la = "ls -la";
-      lt = "eza --group-directories-first --group --tree";
-
-      # Development
-      venv = "virtualenv venv && source venv/bin/activate.fish";
-
-      # Git
-      gpsup = "git push --set-upstream origin \"$(git branch --show-current)\"";
-
-      # Utils
-      cdtmp = "cd (mktemp -d)";
-      mkdate = "mkdir (date +%Y-%m-%d)";
-      nvimdiff = "nvim -d";
-      open = "xdg-open";
-
-      # Config
-      chezmoi-cd = "cd \"$(chezmoi source-path)\"";
-
-      # Bluetooth
-      btconnect = "rfkill unblock bluetooth && bluetoothctl power on && bluetoothctl connect 00:D3:00:00:04:17";
-      btoff = "bluetoothctl power off";
-
-      # Task warrior
-      taskw = "task project:work";
-    };
+    home.shellAliases = aliases // shAliases // ls;
 
     programs = {
       fish.shellAbbrs = abbreviations;
       bash.shellAliases = abbreviations;
       zsh.shellAliases = abbreviations;
+      nushell.shellAliases =
+        abbreviations // aliases // { mkdate = "mkdir (date now | format date %F)"; };
     };
   };
 }

3f1fb4f5d5e95c1284f999ed1414ea6b8d8093c9
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-11-04 23:16:08 +0100

    feat(nvidia): use the card for rendering

--- nixos/nixos/video-configuration.nix
+++ nixos/nixos/video-configuration.nix
@@ -6,15 +6,28 @@
 
     services.xserver.videoDrivers = [ "nvidia" ];
 
+    environment.variables = {
+      DRI_PRIME = "pci-0000_01_00_0";
+      __VK_LAYER_NV_optimus = "NVIDIA_only";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    };
+
     hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.production;
 
       # Used for compute environments
-      nvidiaPersistenced = false;
       modesetting.enable = true;
+
+      # Borked or unsupported
       forceFullCompositionPipeline = false;
+      nvidiaPersistenced = false;
+      dynamicBoost.enable = false;
+
       prime = {
-        sync.enable = true;
+        sync.enable = false;
+
+        # offload.enableOffloadCmd = true;
+
         amdgpuBusId = "PCI:5:0:0";
         nvidiaBusId = "PCI:1:0:0";
       };

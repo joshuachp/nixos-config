505b9020378beb997e567fa97f52abf3db0defb9
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-11-08 13:53:46 +0100

    feat(powersave): enable tlp and disable nvidia

--- nixos/nixos/video-configuration.nix
+++ nixos/nixos/video-configuration.nix
@@ -34,18 +34,61 @@
     };
 
     specialisation = {
-      singleMonitor.configuration = {
-        system.nixos.tags = [ "singleMonitor" ];
+      onTheGo.configuration = {
+        system.nixos.tags = [ "onTheGo" ];
 
         hardware.nvidia = {
+          modesetting.enable = lib.mkForce false;
           prime = {
             offload = {
-              enable = lib.mkForce true;
-              enableOffloadCmd = lib.mkForce true;
+              enable = lib.mkForce false;
+              enableOffloadCmd = lib.mkForce false;
             };
             sync.enable = lib.mkForce false;
           };
         };
+
+        # disable nvidia
+        boot.extraModprobeConfig = ''
+          blacklist nouveau
+          options nouveau modeset=0
+        '';
+
+        boot.blacklistedKernelModules = [
+          "nouveau"
+          "nvidia"
+          "nvidia_drm"
+          "nvidia_modeset"
+        ];
+
+        services = {
+          # battery management
+          power-profiles-daemon.enable = false;
+          tlp = {
+            enable = true;
+            settings = {
+              CPU_SCALING_GOVERNOR_ON_AC = "performance";
+              CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+              CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+              CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+            };
+          };
+
+          # Disable nvidia
+          udev.extraRules = ''
+            # Remove NVIDIA USB xHCI Host Controller devices, if present
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+            # Remove NVIDIA USB Type-C UCSI devices, if present
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+            # Remove NVIDIA Audio devices, if present
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+            # Remove NVIDIA VGA/3D controller devices
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+          '';
+        };
+
+        powerManagement.cpuFreqGovernor = lib.mkForce "ondemand";
       };
     };
   };

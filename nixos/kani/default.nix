ccf42e0930953ab855402daa691a03235277df90
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-12-02 12:13:07 +0100

    feat(kani): add new systemConfig

---
+++ nixos/kani/default.nix
@@ -0,0 +1,56 @@
+{ config, hostname, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disk-config.nix
+  ];
+  config = {
+    systemConfig.minimal = true;
+
+    nixosConfig.networking.resolved = true;
+
+    privateConfig.remote-builder.enable = true;
+
+    services = {
+      openssh = {
+        enable = true;
+        openFirewall = true;
+        settings.PasswordAuthentication = false;
+      };
+      fail2ban.enable = true;
+    };
+
+    users.users.root.openssh.authorizedKeys.keys = [ config.privateConfig.ssh.publicKey ];
+
+    systemd.network.networks = config.lib.config.mkNetworkCfg {
+      "enp1s0" = {
+        networkConfig.MulticastDNS = "yes";
+      };
+      "wlp2s0" = {
+        networkConfig.MulticastDNS = "yes";
+      };
+    };
+    networking.firewall.allowedUDPPorts = [ 5353 ];
+
+    services.resolved = {
+      extraConfig = ''
+        MulticastDNS=true
+      '';
+    };
+
+    services.fwupd.enable = true;
+
+    # Never suspend
+    services.logind.lidSwitch = "ignore";
+
+    nixosConfig.server.k3s = {
+      enable = true;
+      role = "agent";
+      interface = "wg0";
+      ip = "10.0.0.8";
+      externalIp =
+        config.lib.config.wireguard.mkServerIpv4
+          config.privateConfig.machines.${hostname}.wireguard.id;
+    };
+  };
+}
